33fde5453683f754f75754daab87b1e4c28acd0f
The-Ben <ben@theaarons.com>
2018-04-29 21:54:19 -0700

    shrunk existing app for demo

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template, request
+import numpy as np
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+app.vars = {}
+predictor = pickle.load(open('predictor-gd.pkl', 'rb'))
+
+def findSum(a, b, estimator):
+	X = pd.DataFrame([[a,b]])
+	result = estimator.predict(X)
+	return result[0]
+
+@app.route('/', methods =['GET', 'POST'])
+def index():
+	if request.method == 'GET':
+		return render_template('index.html', sum = 0, n1 = 0, n2 = 0)
+
+	else:
+		try:
+			app.vars['n1'] = request.form['number1']
+			app.vars['n2'] = request.form['number2']
+			app.vars['sum'] = findSum(float(app.vars['n1']), float(app.vars['n2']), predictor)
+		except:
+			return render_template('index.html', sum = "%s + %s" % (app.vars['n1'], app.vars['n2']), n1 = app.vars['n1'], n2 = app.vars['n2'])
+		return render_template('index.html', sum = app.vars['sum'], n1 = app.vars['n1'], n2 = app.vars['n2'])
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=33507, debug = True)
---
+++ generate-training-data.py
@@ -0,0 +1,24 @@
+'''
+This is a script that generates N number of test cases.
+each case consists of three comma-delimited values on a line.
+the first value is the sum (result) of the two following 
+randomly generated integers (features) on each line.
+Format:
+sum,int_1,int_2\n
+'''
+
+from random import randint
+import sys
+from tqdm import tqdm
+
+def add_two_random_ints():
+	int_1 = randint(-9,99)
+	int_2 = randint(-9,99)
+	return "%s,%s,%s\n" % (int_1 + int_2, int_1, int_2)
+
+number_cases = abs(int(sys.argv[1]))
+
+with open("cases.csv", "w") as f:
+	f.writelines('A,Q1,Q2\n') 
+	for _ in tqdm(range(number_cases)):
+		f.writelines(add_two_random_ints())

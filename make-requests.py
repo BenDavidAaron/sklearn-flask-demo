0fa3a4d485a04ba3f309952a20190dc97364a1af
The-Ben <ben@theaarons.com>
2018-04-30 09:38:59 -0700

    added a little demo script

--- make-requests.py
+++ make-requests.py
@@ -1,3 +1,14 @@
 import requests
+from random import randint
+from time import sleep
 
-r = requests.get('http://localhost:33507/backdoor/', params = {'x':'5','y':'7'})
+for _ in range(10):
+	print("### New Request")
+	x = randint(-9,19)
+	y = randint(-9,19)
+	print(f"  {x}\n+ {y}\n-----")
+	r = requests.get('http://localhost:33507/backdoor/', params = {'x':x,'y':y})
+	sleep(.5)
+	sum = r.text
+	print(f" {sum}\n")
+	sleep(1)
